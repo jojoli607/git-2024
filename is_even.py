ceada189125e62f6bf946b53bb2c766491d10a37
Lily <cgshyuankai@proton.me>
2024-12-17 10:49:46 +0000

    Create is_even.py

---
+++ is_even.py
@@ -0,0 +1,10 @@
+# is_even.py
+
+def is_even(number):
+    return number % 2 == 0
+
+number = int(input("Enter a number: "))
+if is_even(number):
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
